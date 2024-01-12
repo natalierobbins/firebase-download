c8b4e344d460a5abaed8a7104b3c5f1296cf9764
robbinat <robbinat@umich.edu>
2024-01-12 18:07:20 -0500

    testing

--- colab_script.py
+++ colab_script.py
@@ -1,76 +1,73 @@
-import firebase_admin
-from firebase_admin import credentials, storage
+from firebase_admin import storage
 from tqdm import tqdm
 import requests
-import io
 import os
 from ipytree import Tree, Node
-from google.colab import output
 from collections import defaultdict
 
-def confirm_files(selected):
-  print("\n---------\nBelow are the selected files for download. If you would like to change your files, run this code block again before downloading.\n")
-  for blob in selected:
-    print(blob.name)
-  return(selected)
+# def confirm_files(selected):
+#   print("\n---------\nBelow are the selected files for download. If you would like to change your files, run this code block again before downloading.\n")
+#   for blob in selected:
+#     print(blob.name)
+#   return(selected)
 
-def select_files(app):
-  blobs = list(storage.bucket(app=app).list_blobs())
-  root_dirs = set()
-  nav = []
+# def select_files(app):
+#   blobs = list(storage.bucket(app=app).list_blobs())
+#   root_dirs = set()
+#   nav = []
 
-  depth = 0
+#   depth = 0
 
-  for blob in blobs:
-    split = blob.name.split('/')
-    root_dirs.add(split[0])
+#   for blob in blobs:
+#     split = blob.name.split('/')
+#     root_dirs.add(split[0])
 
-  dir_dict = {}
-  print(f"""List of folders/files at depth {depth}:""")
-  for i, dir in enumerate(sorted(root_dirs)):
-    print(f"""[{i + 1}]""", dir)
-    dir_dict[i + 1] = dir
-  print("Input the number associated with the desired folder/file for download, or input 'all' to select all:\n")
-  selected = input().strip()
-  print("\n---------\n")
+#   dir_dict = {}
+#   print(f"""List of folders/files at depth {depth}:""")
+#   for i, dir in enumerate(sorted(root_dirs)):
+#     print(f"""[{i + 1}]""", dir)
+#     dir_dict[i + 1] = dir
+#   print("Input the number associated with the desired folder/file for download, or input 'all' to select all:\n")
+#   selected = input().strip()
+#   print("\n---------\n")
 
-  done = False
-  while not done:
-    selected_dirs = set()
-    if selected == "all":
-        for blob in blobs:
-          if blob.name.split('/')[:depth] == nav:
-            if blob.name.split('/')[depth]:
-              selected_dirs.add(blob.name.split('/')[depth])
-        done = True
-    else:
-      try:
-        nav.append(dir_dict[int(selected)])
-        depth += 1
-        for blob in blobs:
-          if blob.name.split('/')[:depth] == nav:
-            if blob.name.split('/')[depth]:
-              selected_dirs.add(blob.name.split('/')[depth])
-        print(f"""List of directories at depth {depth}:""")
-        for i, dir in enumerate(sorted(selected_dirs)):
-          print(f"""[{i + 1}]""", dir)
-          dir_dict[i + 1] = dir
-        print("Input the number associated with the desired folder/file for download, or input 'all' to select all:\n")
-        selected = input().strip()
-        print("\n---------\n")
-      except:
-        print("Please input a valid number or 'all'")
-        selected = input().strip()
+#   done = False
+#   while not done:
+#     selected_dirs = set()
+#     if selected == "all":
+#         for blob in blobs:
+#           if blob.name.split('/')[:depth] == nav:
+#             if blob.name.split('/')[depth]:
+#               selected_dirs.add(blob.name.split('/')[depth])
+#         done = True
+#     else:
+#       try:
+#         nav.append(dir_dict[int(selected)])
+#         depth += 1
+#         for blob in blobs:
+#           if blob.name.split('/')[:depth] == nav:
+#             if blob.name.split('/')[depth]:
+#               selected_dirs.add(blob.name.split('/')[depth])
+#         print(f"""List of directories at depth {depth}:""")
+#         for i, dir in enumerate(sorted(selected_dirs)):
+#           print(f"""[{i + 1}]""", dir)
+#           dir_dict[i + 1] = dir
+#         print("Input the number associated with the desired folder/file for download, or input 'all' to select all:\n")
+#         selected = input().strip()
+#         print("\n---------\n")
+#       except:
+#         print("Please input a valid number or 'all'")
+#         selected = input().strip()
 
-  selected_blobs = []
-  for dir in selected_dirs:
-    navCopy = [x for x in nav]
-    navCopy.append(dir)
-    for blob in blobs:
-      if blob.name.split('/')[:len(navCopy)] == navCopy:
-        selected_blobs.append(blob)
+#   selected_blobs = []
+#   for dir in selected_dirs:
+#     navCopy = [x for x in nav]
+#     navCopy.append(dir)
+#     for blob in blobs:
+#       if blob.name.split('/')[:len(navCopy)] == navCopy:
+#         selected_blobs.append(blob)
 
-  return selected_blobs
+#   return selected_blobs
   
 def traverse(curr_node, filenames):
   # if not leaf node
